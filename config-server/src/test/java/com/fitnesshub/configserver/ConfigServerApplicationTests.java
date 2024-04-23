f5260326cc06c0863dd95e6e9dfdc86e6323b603
Naro48 <nassim_feddouli@um5.ac.ma>
2024-04-23 18:38:22 +0000

    Initialisation du config server

---
+++ config-server/src/test/java/com/fitnesshub/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitnesshub.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
